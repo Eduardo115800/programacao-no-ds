947c2c6978c2cc9764cd2eeb887c39cfb3405416
Eduardo115800 <eduardo.maciel.barbosa@escola.pr.gov.br>
2025-06-05 11:59:05 -0300

    Create contagem.py

---
+++ contagem.py
@@ -0,0 +1,8 @@
+# Solicita ao usuário que insira uma frase
+frase = input("Digite uma frase: ")
+https://www.onlinegdb.com/#editor_1
+# Conta o número de caracteres na frase
+num_caracteres = len(frase)
+
+# Exibe o resultado
+print(f"A frase contém {num_caracteres} caracteres.")
